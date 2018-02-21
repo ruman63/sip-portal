207e067688965f38d1ca9068eb6791ec1a0addc6
Rumman <rumman@arch-pc-rumman.desktop>
2018-02-21 17:01:42 +0530

    Add UI to indicate Logged in as Client

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,3 +1,22 @@
+@if(auth()->guard('web')->check() && auth()->guard('cpanel')->check())
+    <nav class="bg-grey-darker text-white">
+        <div class="container mx-auto">
+            <div class="flex">
+                <div class="flex-1 flex items-center">
+                    You are logged in as {{ auth()->guard('web')->user()->first_name }} ( {{ auth()->guard('web')->user()->email }} )
+                </div>
+                <div class="flex flex-row-reverse items-center">
+                    <form action="{{ route('clients.logout') }}" method="POST">
+                        {{ csrf_field() }}
+                        <button type="submit" class="bg-red-dark py-2 px-3 text-white"> 
+                            <i class="fa fa-times mr-1"></i> Exit Access 
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
+@endif
 <nav class="bg-teal-dark text-white">
     <div class="container mx-auto flex">
         <div class="flex flex-1 items-center">
@@ -11,16 +30,18 @@
             <li>
                 <dropdown heading="Hello! {{ Auth::user()->first_name }}">
                     <template slot="button">
-                        <span class="relative z-10 p-1 bg-pink-darkest text-white flex items-center justify-center rounded-full w-8 h-8">
+                        <span class="relative z-10 p-1 bg-teal-darkest text-white flex items-center justify-center rounded-full w-8 h-8">
                             <i class="fa fa-user"></i>
                         </span>
                     </template>
                     <template slot="menu">
                         <li class="py-1"><a href="#">Profile</a></li>
                         <li class="py-1"><a href="#">Change Password</a></li>
-                        <li class="py-1">
-                            <logout path="{{ route('logout') }}"></logout>
-                        </li>
+                        @if(auth()->guard('cpanel')->guest())
+                            <li class="py-1">
+                                <logout path="{{ route('logout') }}"></logout>
+                            </li>
+                        @endif
                     </template>
                 </dropdown>
             </li>
