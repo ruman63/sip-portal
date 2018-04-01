6764cc8b580a7964299787a038a047901aa95a1c
Ruman Saleem <ruman63@gmail.com>
2018-04-01 23:50:33 +0530

    Increase Amount Size

--- database/migrations/2018_04_01_065445_create_transactions_table.php
+++ database/migrations/2018_04_01_065445_create_transactions_table.php
@@ -17,8 +17,8 @@ class CreateTransactionsTable extends Migration
             $table->increments('id');
             $table->string('uid')->unique();
             $table->string('type');
-            $table->float('rate')->nullable();
-            $table->float('amount');
+            $table->float('rate', 6)->nullable();
+            $table->float('amount', 16);
             $table->string('folio_id');
             $table->string('date');
             $table->timestamps();
