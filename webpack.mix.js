fd77289310fb89e0872551f9e8cfb82ae5b4e5f5
Ruman Saleem <ruman63@gmail.com>
2018-05-31 04:53:33 +0530

    added Purge Css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,37 @@
 let mix = require('laravel-mix');
 var tailwindcss = require('tailwindcss');
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+let glob = require("glob-all");
+let PurgecssPlugin = require("purgecss-webpack-plugin");
 
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
+
+if(mix.inProduction()){
+    mix.webpackConfig({
+        plugins: [
+          new PurgecssPlugin({
+            // Specify the locations of any files you want to scan for class names.
+            paths: glob.sync([
+              path.join(__dirname, "resources/views/**/*.blade.php"),
+              path.join(__dirname, "resources/assets/js/**/*.vue"),
+              path.join(__dirname, "public/js/main.js")
+            ]),
+            extractors: [
+              {
+                extractor: TailwindExtractor,
+    
+                // Specify the file extensions to include when scanning for
+                // class names.
+                extensions: ["html", "js", "php", "vue"]
+              }
+            ]
+          })
+        ]
+    });
+}
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .options({
