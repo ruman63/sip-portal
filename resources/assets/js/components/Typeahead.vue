d613c1cf57015c5c5504aea6c0f2e169ec5598bd
Ruman Saleem <ruman63@gmail.com>
2018-06-10 04:16:20 +0530

    Typeahead wip

--- resources/assets/js/components/Typeahead.vue
+++ resources/assets/js/components/Typeahead.vue
@@ -1,119 +1,109 @@
 <template>
-  <div>
-    <!-- optional indicators -->
-    <!-- <i class="absolute  fa fa-spinner fa-spin" v-if="loading"></i> -->
+    <div @blur="close">
+        <button class="border px-3 py-2 w-full text-left focus:border-blue-darker" @click="toggle">
+            <span v-if="isSelected">
+                <slot name="selectedItem" :selected="items[selectedIndex]">{{ items[selectedIndex] }}</slot>
+            </span>
+            <span v-else class="text-grey-dark" v-text="placeholder"></span>
+        </button>
+        <div v-show="isOpen" class="relative">
+            <div class="absolute border mt-2 pin-t pin-x flex flex-col p-3">
+                <input ref="input" 
+                  type="text"
+                  v-model="query"
+                  @input="input"
+                  @keydown.down="highlightNext"
+                  @keydown.up="highlightPrev"
+                  class="control"
+                  autofocus/>
+                <ul ref="options" v-if="items.length > 0" class="list-reset mt-2 h-64 overflow-x-scroll">
+                    <li v-for="(item,i) in items" :key="i"
+                        class="py-2 px-3 cursor-pointer rounded" 
+                        :class="{'bg-blue-darker text-white':isHighlighted(i)}"
+                        @click="select(i)"
+                        @mouseover="highlight(i)">
+                        <slot :item="item">
+                        </slot>
+                    </li>
+                </ul>
+            </div>
 
-    <!-- the input field -->
-    <input type="text"
-           class="control"
-           placeholder="Search Schemes..."
-           autocomplete="off"
-           v-model="query"
-           @keydown.down="down"
-           @keydown.up="up"
-           @keydown.enter.prevent="hit"
-           @keydown.esc="reset"
-           @blur="blur"
-           @focus="focus"
-           @input="update"/>
-
-    <input type="hidden" :name="name" v-model="data.scheme_code">
-
-    <!-- the list -->
-    <div v-show="show" class="relative">
-        <ul v-if="hasItems" 
-        ref="options"
-        class="absolute pin-t pin-x list-reset p-1 bg-white border h-64 overflow-y-scroll mt-1 border-solid border-grey">
-          <!-- for vue@1.0 use: ($item, item) -->
-          <li v-for="(item, $item) in items" 
-            :key="item.unique_no"
-            :class="activeClass($item)" 
-            class="py-1 px-3 rounded"
-            @mousedown="hit" 
-            @mousemove="setActive($item)">
-              <span v-text="`[${item.scheme_code}] ${item.scheme_name}`"></span>
-          </li>
-        </ul>
-        <div v-else 
-          v-text="empty"
-          class="absolute pin-t pin-x p-1 bg-white border mt-1 border-solid border-grey">
         </div>
     </div>
-  </div>
 </template>
 
 <script>
-import VueTypeahead from '../mixins/Typeahead';
+import debounce from 'lodash.debounce';
 export default {
-  extends: VueTypeahead, // vue@1.0.22+
-  // mixins: [VueTypeahead], // vue@1.0.21-
-  props: {
-    'url':{},
-    'name':{},
-    'value':{}
-  },
-  data () {
-    return {
-      // The source url
-      // (required)
-      src: this.url,
-
-      // The data that would be sent by request
-      // (optional)
-      data: {},
-
-      // Limit the number of items which is shown at the list
-      // (optional)
-      // limit: 5,
-
-      // The minimum character length needed before triggering
-      // (optional)
-      minChars: 3,
-
-      time: 300,
-
-      // Highlight the first item in the list
-      // (optional)
-      selectFirst: false,
-
-      // Override the default value (`q`) of query parameter name
-      // Use a falsy value for RESTful query
-      // (optional)
-      queryParamName: 's'
-    }
-  },
-  watch: {
-    value() {
-      if(this.value) {
-        this.onHit(this.value);
-      }
-    }
-  },
-  methods: {
-    // The callback function which is triggered when the user hits on an item
-    // (required)
-    onHit (item) {
-      this.data = item;
-      this.query = `[${this.data.scheme_code}] ${this.data.scheme_name}`;
-      this.$emit('input', item);
+    props: {
+        placeholder: { default: "Select an Item..." },
+        src: {required: true }
     },
-
-    // The callback function which is triggered when the response data are received
-    // (optional)
-    prepareResponseData (data) {
-      // data = ...
-      return data;
+    data() {
+        return {
+            query: '',
+            isOpen: false,
+            items: [],
+            selectedIndex: -1,
+            highlightedIndex: -1,
+        }
     },
-
-    activeClass(index) {
-        return index === this.current ? 'bg-blue-darkest text-white' : 'text-black';
+    methods:{
+        input: debounce(function() {
+            this.makeRequest().then(({data}) => this.items = data);
+        }, 500),
+        makeRequest() {
+            return axios.get(this.src, {params:{ s:this.query } });
+        },
+        toggle() {
+            if(this.isOpen) {
+                this.close();
+            } else {
+                this.open();
+            }
+        },
+        open() {
+            this.isOpen = true;
+            this.$nextTick(() => this.$refs.input.focus() );
+        },
+        close() {
+            this.isOpen = false;
+        },
+        select(i) {
+            if( i >= 0 && i<this.items.length ) {
+                this.selectedIndex = i;
+                this.close();
+            }
+        },
+        isSelected() {
+            return this.selectedIndex >= 0;
+        },
+        highlight(i) {
+            this.highlightedIndex = i;
+            if(this.highlightedIndex >= this.items.length ) {
+                this.highlightedIndex = this.items.length-1;
+            }
+            if(this.highlightedIndex < 0) {
+                this.highlightedIndex = 0;
+            }
+            this.$refs.options.children[this.highlightedIndex].scrollIntoView({ block: 'nearest' });
+        },
+        highlightNext() {
+            this.highlight(this.highlightedIndex + 1);
+        },
+        highlightPrev() {
+            this.highlight(this.highlightedIndex - 1);
+        },
+        isHighlighted(i) {
+            if(i != null) {
+                return i==this.highlightedIndex;
+            }
+            return this.highlightedIndex >= 0;
+        }
+    },
+    mounted() {
+        axios.get(this.src).then(({data}) => this.items = data);
     }
-  },
-  mounted() {
-    if(this.value) {
-      this.onHit(this.value);
-    }
-  }
-}
+};
 </script>
 
